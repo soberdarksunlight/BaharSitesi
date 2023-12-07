b89c48de74d64b5f71fe3b67b98e7b4ef4625722
Ahmetcan Onsekiz <ahmetcanonsekiz@gmail.com>
2023-12-07 20:40:41 +0300

    Daire Modeli Eklenmiş Hali

---
+++ WebApplication1/Models/Daire.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Daire
+    {
+        public int Id { get; set; }
+        public int DaireNo {  get; set; }
+        public string Blok {  get; set; }
+        public int Kat {  get; set; }
+    }
+}
